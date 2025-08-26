8e90e07d40e7370ccde3960ec2da6ff78a35a4c7
Yead Ahmed <ahmedyead10@gmail.com>
2025-08-26 21:17:20 +0600

    Final version: clickable links & Render PORT support

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request
-from scraper import get_scholarships
-
-import os  # For Render PORT environment variable
+from scraper import search_scholarships
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/search", methods=["POST"])
+@app.route('/search', methods=['POST'])
 def search():
-    query = request.form.get("query")
-    results = get_scholarships(query)
+    query = request.form['query']
+    results = search_scholarships(query)
     return render_template("index.html", results=results, query=query)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
-    app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
